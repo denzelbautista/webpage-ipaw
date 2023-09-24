06b739f507ce210823dba5793bffc857a3972c83
campama <129806557+campama@users.noreply.github.com>
2023-09-24 17:15:22 -0500

    separación en 3 apis

--- backend/api_mascota.py
+++ backend/api_mascota.py
@@ -1,4 +1,7 @@
 
+
+#api mascotas 
+
 from flask import Flask, jsonify,  request
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
@@ -16,9 +19,17 @@ class Usuario(db.Model):
     __tablename__ = 'usuario'
     dni = db.Column(db.BigInteger, primary_key=True)
     nombre = db.Column(db.String(30), nullable=False)
-    contrasenia = db.Column(db.String(60), nullable=False)
     apellido = db.Column(db.String(40), nullable=False)
     direccion = db.Column(db.String(100), nullable=False)
+    def __repr__(self):
+        return '<Usuario %r>' % self.nombre
+    def serialize(self):
+        return {
+            "dni": self.dni,
+            "nombre": self.nombre,
+            "apellido": self.apellido,
+            "direccion": self.direccion
+        }
 
 class Mascota(db.Model):
     __tablename__ = 'mascota'
@@ -28,33 +39,24 @@ class Mascota(db.Model):
     animal = db.Column(db.String(30), nullable=False)
     raza = db.Column(db.String(30), nullable=False)
     usuario = db.relationship('Usuario', foreign_keys=[dni_usuario])
+    def __repr__(self):
+        return '<Mascota %r>' % self.nombre
+    def serialize(self):
+        return {
+            "id": self.id,
+            "dni_usuario": self.dni_usuario,
+            "nombre": self.nombre,
+            "animal": self.animal,
+            "raza": self.raza,
+        }
 
-class Reserva(db.Model):
-    __tablename__ = 'reserva'
-    numero_reserva = db.Column(db.Integer, primary_key=True)
-    dni_usuario = db.Column(db.BigInteger, db.ForeignKey('usuario.dni'))
-    id_mascota = db.Column(db.Integer, db.ForeignKey('mascota.id'))
-    servicio = db.Column(db.String(30), nullable=False)
-    f_inicio = db.Column(db.Date, nullable=False)
-    f_fin = db.Column(db.Date, nullable=False)
-
-    
-    usuario = db.relationship('Usuario', foreign_keys=[dni_usuario])
-    mascota = db.relationship('Mascota', foreign_keys=[id_mascota])
-
-class Ventas(db.Model):
-    __tablename__ = 'ventas'
-    numero_orden = db.Column(db.Integer, primary_key=True)
-    dni_usuario = db.Column(db.BigInteger, db.ForeignKey('usuario.dni'))
-    producto = db.Column(db.String(30), nullable=False)
-    fecha_orden = db.Column(db.Date, nullable=False)
-    usuario = db.relationship('Usuario', foreign_keys=[dni_usuario])
 
 @app.route('/mascotas', methods=["GET", "POST"])
 def get_post_mascotas():
   if request.method == "GET":
     mascotas = Mascota.query.all()
-    return jsonify(mascotas)
+    mascotas_serialize = [mascota.serialize() for mascota in mascotas]
+    return jsonify({'succes':True, 'data': mascotas_serialize})
   
   elif request.method == "POST":
     nueva_mascota = Mascota(dni_usuario = request.get_json()['dni_usuario'], nombre = request.get_json()['nombre'], animal = request.get_json()['animal'], raza = request.get_json()['raza'])
@@ -71,7 +73,7 @@ def get_put_delete_mascotas(id):
       db.session.commit()
 
   elif request.method == "GET":
-     return jsonify(mascota)
+     return jsonify(mascota.serialize())
   
   elif request.method == "UPDATE":
      mascota.dni_usuario = request.get_json()['dni_usuario']
