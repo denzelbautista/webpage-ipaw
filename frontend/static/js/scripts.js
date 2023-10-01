1545537076baedf8c6134097aad67eb54758a168
campama <129806557+campama@users.noreply.github.com>
2023-10-01 08:07:19 -0500

    Update scripts.js

--- frontend/static/js/scripts.js
+++ frontend/static/js/scripts.js
@@ -2,6 +2,10 @@ function redirigirlogin() {
   window.location.href = "/login";
 }
 
+function redirigirregistro_m_perdidas() {
+  window.location.href = "/registro_m_perdidas";
+}
+
 function redirigirregistro() {
   window.location.href = "/registro";
 }
