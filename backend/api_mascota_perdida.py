391a0475421b610e155a517e191b2943daf49463
campama <129806557+campama@users.noreply.github.com>
2023-10-01 08:36:21 -0500

    Update api_mascota_perdida.py

--- backend/api_mascota_perdida.py
+++ backend/api_mascota_perdida.py
@@ -5,6 +5,7 @@ from flask import Flask, jsonify,  request
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 app = Flask(__name__)
+CORS(app, origins='http://127.0.0.1:5000')
 # Configura la conexión a la base de datos
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:utec@localhost:3306/ipaw'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
