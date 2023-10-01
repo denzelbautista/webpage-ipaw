025fceeab4c7c65e0e94acc517e50712a3c3077c
Denzel Ysai Bautista Rodriguez <129910983+denzelbautista@users.noreply.github.com>
2023-10-01 11:45:31 -0500

    Refactor api_mascota_perdida.py

--- backend/api_mascota_perdida.py
+++ backend/api_mascota_perdida.py
@@ -1,21 +1,20 @@
-# api mascotas perdidas
 import uuid
 import requests
-from flask import Flask, jsonify,  request
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+
 app = Flask(__name__)
-CORS(app, origins='http://127.0.0.1:5000')
+CORS(app, origins='*')
+
 # Configura la conexión a la base de datos
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:utec@localhost:3306/ipaw'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://admin:oDSEzjdaim3MSgGjYbm1@database-1.cwsnkubwbttj.us-east-1.rds.amazonaws.com:3306/ipaw'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Crea una instancia de SQLAlchemy
 db = SQLAlchemy(app)
 
-
-# modelos
-# por si acaso el del usuario aunque aqui se puede eliminar dependiendo si agregan una ruta que lo necesite :)
+# Modelos
 class Usuario(db.Model):
     __tablename__ = 'usuario'
     dni = db.Column(db.BigInteger, primary_key=True)
@@ -36,8 +35,7 @@ class Usuario(db.Model):
             "direccion": self.direccion
         }
 
-
-class Mascotas_perdidas(db.Model):
+class MascotasPerdidas(db.Model):
     __tablename__ = 'mascotas_perdidas'
     id = db.Column(db.String(36), primary_key=True, unique=True, default=lambda: str(
         uuid.uuid4()), server_default=db.text("uuid_generate_v4()"))
@@ -65,22 +63,22 @@ class Mascotas_perdidas(db.Model):
             "estado": self.estado
         }
 
-
 @app.route('/mascotas_perdidas', methods=["POST"])
-def create_mascotaperdida():
+def create_mascota_perdida():
     dni_usuario = request.form.get('dni_usuario')
     nombre = request.form.get('nombre')
     animal = request.form.get('animal')
     raza = request.form.get('raza')
     descripcion = request.form.get('descripcion')
     estado = request.form.get('estado')
+    
     if 'image' not in request.files:
-        return 'No image uploaded!', 400
+        return jsonify({'success': False, 'message': 'No image uploaded!'}), 400
 
     file = request.files['image']
 
     if file.filename == '':
-        return 'No image selected!', 400
+        return jsonify({'success': False, 'message': 'No image selected!'}), 400
 
     # Subimos la imagen al servicio de imgBB y obtenemos el nombre del archivo
     response = requests.post('https://api.imgbb.com/1/upload',
@@ -90,41 +88,37 @@ def create_mascotaperdida():
     image_url = response.json()['data']['display_url']
 
     # Guardamos el URL de la imagen en el atributo imagen de la base de datos
-
-    mascota_p = Mascotas_perdidas(dni_usuario=dni_usuario, nombre=nombre, animal=animal, raza=raza,
-                                  descripcion=descripcion, estado=estado, image=image_url)
-    db.session.add(mascota_p)
-
+    mascota_perdida = MascotasPerdidas(dni_usuario=dni_usuario, nombre=nombre, animal=animal, raza=raza,
+                                       descripcion=descripcion, estado=estado, image=image_url)
+    db.session.add(mascota_perdida)
     db.session.commit()
 
-    return 'Mascota perdida agregada :C', 201
-
+    return jsonify({'success': True, 'message': 'Mascota perdida agregada', 'data': mascota_perdida.serialize()}), 201
 
 @app.route('/mascotas_perdidas', methods=["GET"])
-def get_post_mascotas_perdidas():
-
-    mascotas = Mascotas_perdidas.query.all()
+def get_mascotas_perdidas():
+    mascotas = MascotasPerdidas.query.all()
     mascotas_serialize = [mascota.serialize() for mascota in mascotas]
-    return jsonify({'succes': True, 'data': mascotas_serialize})
-
+    return jsonify({'success': True, 'data': mascotas_serialize})
 
 @app.route('/mascotas_perdidas/<id>', methods=['GET', 'PUT', 'DELETE'])
-def get_put_delete_mascotas_perdidas(id):
-    mascota = Mascotas_perdidas.query.get_or_404(id)
+def get_put_delete_mascota_perdida(id):
+    mascota = MascotasPerdidas.query.get_or_404(id)
+
     if request.method == "DELETE":
         db.session.delete(mascota)
         db.session.commit()
+        return jsonify({'success': True, 'message': 'Mascota perdida eliminada'}), 204
 
     elif request.method == "GET":
-        return jsonify(mascota.serialize())
+        return jsonify({'success': True, 'data': mascota.serialize()})
 
-    elif request.method == "UPDATE":  # para el boton de encontrado
+    elif request.method == "PUT":
         mascota.estado = "encontrado"
         db.session.commit()
-        return 'Mascota perdida ha sido encontrada :)', 201
+        return jsonify({'success': True, 'message': 'Mascota perdida encontrada'}), 201
 
     return 'SUCCESS'
 
-
 if __name__ == '__main__':
-    app.run(port=5003)
+    app.run(host='0.0.0.0',port=5003)
