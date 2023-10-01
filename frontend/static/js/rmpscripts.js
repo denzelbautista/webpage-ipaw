bd8013752cb786d6f3890d962e926a03b1cfdd52
campama <129806557+campama@users.noreply.github.com>
2023-10-01 11:34:00 -0500

    Update rmpscripts.js

--- frontend/static/js/rmpscripts.js
+++ frontend/static/js/rmpscripts.js
@@ -36,3 +36,19 @@ document.addEventListener("DOMContentLoaded", function () {
       });
   });
 });
+
+
+function redirigirregistro_m_perdidas() {
+  window.location.href = "/registro_m_perdidas";
+}
+
+function redirigirregistro() {
+  window.location.href = "/registro";
+}
+
+function redirigirmascota() {
+  window.location.href = "/registro_m";
+}
+function redirigirreserva() {
+  window.location.href = "/reserva";
+}
