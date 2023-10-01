bdda1426b00e0595e453adfb3e1d5e59f7cff341
Denzel Ysai Bautista Rodriguez <129910983+denzelbautista@users.noreply.github.com>
2023-10-01 11:44:47 -0500

    Refactor api_mascota.py

--- backend/api_mascota.py
+++ backend/api_mascota.py
@@ -1,20 +1,19 @@
-from flask import Flask, jsonify,  request
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-app = Flask(__name__)
 import uuid
 
-CORS(app, origins='http://127.0.0.1:5000')
+app = Flask(__name__)
+CORS(app, origins='*')
 
 # Configura la conexión a la base de datos
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:utec@localhost:3306/ipaw'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://admin:oDSEzjdaim3MSgGjYbm1@database-1.cwsnkubwbttj.us-east-1.rds.amazonaws.com:3306/ipaw'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Crea una instancia de SQLAlchemy
 db = SQLAlchemy(app)
 
-
-#modelos
+# Modelos
 class Usuario(db.Model):
     __tablename__ = 'usuario'
     dni = db.Column(db.BigInteger, primary_key=True)
@@ -22,20 +21,22 @@ class Usuario(db.Model):
     contrasenia = db.Column(db.String(60), nullable=False)
     apellido = db.Column(db.String(40), nullable=False)
     direccion = db.Column(db.String(100), nullable=False)
+
     def __repr__(self):
         return '<Usuario %r>' % self.nombre
+
     def serialize(self):
         return {
             "dni": self.dni,
             "nombre": self.nombre,
             "apellido": self.apellido,
-            "contrasenia":self.contrasenia,
+            "contrasenia": self.contrasenia,
             "direccion": self.direccion
         }
 
 class Mascota(db.Model):
     __tablename__ = 'mascota'
-    id = db.Column(db.String(36),primary_key=True,unique=True,default=lambda: str(uuid.uuid4()), server_default=db.text("uuid_generate_v4()"))
+    id = db.Column(db.String(36), primary_key=True, unique=True, default=lambda: str(uuid.uuid4()), server_default=db.text("uuid_generate_v4()"))
     dni_usuario = db.Column(db.BigInteger, db.ForeignKey('usuario.dni'))
     nombre = db.Column(db.String(30), nullable=False)
     animal = db.Column(db.String(30), nullable=False)
@@ -44,6 +45,7 @@ class Mascota(db.Model):
 
     def __repr__(self):
         return '<Mascota %r>' % self.nombre
+
     def serialize(self):
         return {
             "id": self.id,
@@ -53,38 +55,40 @@ class Mascota(db.Model):
             "raza": self.raza
         }
 
-
 @app.route('/mascotas', methods=["GET", "POST"])
 def get_post_mascotas():
-  if request.method == "GET":
-    mascotas = Mascota.query.all()
-    mascotas_serialize = [mascotas.serialize() for mascotas in mascotas]
-    return jsonify({'success':True, 'data':mascotas_serialize})
+    if request.method == "GET":
+        mascotas = Mascota.query.all()
+        mascotas_serialize = [mascota.serialize() for mascota in mascotas]
+        return jsonify({'success': True, 'data': mascotas_serialize})
 
-  elif request.method == "POST":
-    nueva_mascota = Mascota(dni_usuario = request.get_json()['dni_usuario'], nombre = request.get_json()['nombre'], animal = request.get_json()['animal'], raza = request.get_json()['raza'])
-    db.session.add(nueva_mascota)
-    db.session.commit()
-    return 'Mascota creada :)', 201
+    elif request.method == "POST":
+        data = request.get_json()
+        nueva_mascota = Mascota(dni_usuario=data['dni_usuario'], nombre=data['nombre'], animal=data['animal'], raza=data['raza'])
+        db.session.add(nueva_mascota)
+        db.session.commit()
+        return jsonify({'success': True, 'message': 'Mascota creada', 'data': nueva_mascota.serialize()}), 201
 
-@app.route('/mascotas/<id>', methods = ['GET', 'PUT', 'DELETE'])
+@app.route('/mascotas/<id>', methods=['GET', 'PUT', 'DELETE'])
 def get_put_delete_mascotas(id):
-  mascota  = Mascota.query.get_or_404(id)
-  if request.method == "DELETE":
-      db.session.delete(mascota)
-      db.session.commit()
+    mascota = Mascota.query.get_or_404(id)
 
-  elif request.method == "GET":
-     return jsonify(mascota.serialize())
+    if request.method == "DELETE":
+        db.session.delete(mascota)
+        db.session.commit()
+        return jsonify({'success': True, 'message': 'Mascota eliminada'}), 204
 
-  elif request.method == "UPDATE":
-     mascota.dni_usuario = request.get_json()['dni_usuario']
-     mascota.nombre = request.get_json()['nombre']
-     mascota.animal = request.get_json()['animal']
-     mascota.raza = request.get_json()['raza']
-     db.session.commit()
+    elif request.method == "GET":
+        return jsonify({'success': True, 'data': mascota.serialize()})
 
-  return 'SUCCESS'
+    elif request.method == "PUT":
+        data = request.get_json()
+        mascota.dni_usuario = data['dni_usuario']
+        mascota.nombre = data['nombre']
+        mascota.animal = data['animal']
+        mascota.raza = data['raza']
+        db.session.commit()
+        return jsonify({'success': True, 'message': 'Mascota actualizada', 'data': mascota.serialize()}), 200
 
-if __name__ =='__main__':
-    app.run(port=5002)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5002)
